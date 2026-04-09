581d3dad3b440fee69c88635cfe2c5e6371112bb
ew5234 <ericwong0620168@gmail.com>
2026-04-09 01:20:47 -0700

    Initial Test of World Generation with Grass and Water

---
+++ scripts/world_gen.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+@export var noise_height_texture : NoiseTexture2D
+var noise : Noise
+
+@onready var grassTileMap = $TileMap/grass
+@onready var sandTileMap = $TileMap/sand
+@onready var waterTileMap = $TileMap/water
+
+var sourceid = 0
+var wateratlas = Vector2i(5,2)
+var landatlas =  Vector2i(4,4)
+
+var grassTileCoor = []
+var grassTerrainInt = 0
+var sandTileCoor = []
+var waterTileCoor = []
+
+#world size
+var width : int = 100
+var height : int = 100
+
+
+func _ready():
+	noise = noise_height_texture.noise
+	generate_world()
+	
+func generate_world():
+	for x in range(width):
+		for y in range(height):
+			var noise_val = noise.get_noise_2d(x,y)
+			#place grass
+			if noise_val > 0.0:
+				grassTileCoor.append(Vector2i(x,y))
+			#place water
+			elif noise_val	< 0.0:
+				waterTileMap.set_cell(Vector2(x,y), sourceid, wateratlas)
+			
+	grassTileMap.set_cells_terrain_connect(grassTileCoor, grassTerrainInt, 0)
+	
+	
+	
+	
